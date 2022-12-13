dcebbd7c5701c237a04e660e901b7da3c9bc74fe
HamSoThuan1 <hungntph21758@fpt.edu.vn>
2022-12-13 22:03:11 +0700

    update

--- src/repository/thongke_repositoty.java
+++ src/repository/thongke_repositoty.java
@@ -17,124 +17,131 @@ import java.util.logging.Logger;
  * @author QUANGMINH
  */
 public class thongke_repositoty {
-    public static List<thongke> getallthongke(){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON";
-    rs=JDBC_Helper.selectTongQuat(sql);
+
+    public static List<thongke> getallthongke() {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON";
+        rs = JDBC_Helper.selectTongQuat(sql);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
-    public static List<thongke> getthongketheongay(){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON where day(NgayTao)=(DATEPART(day,GETDATE())) and month(NgayTao)=(DATEPART(MONTH,GETDATE()))and year(NgayTao)=(DATEPART(year,GETDATE()))";
-    rs=JDBC_Helper.selectTongQuat(sql);
+
+    public static List<thongke> getthongketheongay() {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON where day(NgayTao)=(DATEPART(day,GETDATE())) and month(NgayTao)=(DATEPART(MONTH,GETDATE()))and year(NgayTao)=(DATEPART(year,GETDATE()))";
+        rs = JDBC_Helper.selectTongQuat(sql);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
-    public static List<thongke> getthongketheothang(){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON where month(NgayTao)=(DATEPART(MONTH,GETDATE()))and year(NgayTao)=(DATEPART(year,GETDATE()))";
-    rs=JDBC_Helper.selectTongQuat(sql);
+
+    public static List<thongke> getthongketheothang() {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON where month(NgayTao)=(DATEPART(MONTH,GETDATE()))and year(NgayTao)=(DATEPART(year,GETDATE()))";
+        rs = JDBC_Helper.selectTongQuat(sql);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
-    public static List<thongke> getthongketheonam(){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON where year(NgayTao)=(DATEPART(year,GETDATE()))";
-    rs=JDBC_Helper.selectTongQuat(sql);
+
+    public static List<thongke> getthongketheonam() {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON where year(NgayTao)=(DATEPART(year,GETDATE()))";
+        rs = JDBC_Helper.selectTongQuat(sql);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
-    public static List<thongke> getthongkebythang(String thang,String nam){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON where month(NgayTao)=? and YEAR(NgayTao)=?";
-    rs=JDBC_Helper.selectTongQuat(sql,thang,nam);
+
+    public static List<thongke> getthongkebythang(String thang, String nam) {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON where month(NgayTao)=? and YEAR(NgayTao)=?";
+        rs = JDBC_Helper.selectTongQuat(sql, thang, nam);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
-    public static List<thongke> getthongkebynam(String nam){
-    List<thongke> listtk = new ArrayList<>();
-    ResultSet rs;
-    String sql="select MaHD,NgayTao,TongTien,TrangThai from HOADON where YEAR(NgayTao)=?";
-    rs=JDBC_Helper.selectTongQuat(sql,nam);
+
+    public static List<thongke> getthongkebynam(String nam) {
+        List<thongke> listtk = new ArrayList<>();
+        ResultSet rs;
+        String sql = "select MaHD,NgayTao,TongTien,TrangThai from HOADON where YEAR(NgayTao)=?";
+        rs = JDBC_Helper.selectTongQuat(sql, nam);
         try {
-            while(rs.next()){
-            String mahd=rs.getString(1);
-            Date ngaytao=rs.getDate(2);
-            double tongtien=rs.getDouble(3);
-            int trangthai=rs.getInt(4);
-            thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
-            listtk.add(tk);
+            while (rs.next()) {
+                String mahd = rs.getString(1);
+                Date ngaytao = rs.getDate(2);
+                double tongtien = rs.getDouble(3);
+                int trangthai = rs.getInt(4);
+                thongke tk = new thongke(mahd, ngaytao, tongtien, trangthai);
+                listtk.add(tk);
             }
             return listtk;
         } catch (SQLException ex) {
             return null;
         }
     }
+
     public static void main(String[] args) {
-         List<thongke>list= getthongketheonam();
-         for (thongke x : list) {
-             System.out.println(x.toString());
+        List<thongke> list = getthongketheonam();
+        for (thongke x : list) {
+            System.out.println(x.toString());
         }
     }
 }
